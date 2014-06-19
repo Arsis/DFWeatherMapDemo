69352b0a76d812d171131571429872d1e0891874
Arsis <dmitry.fedorov@dfedorov.sf-office>
2014-06-19 15:11:43 +0400

    created empty project

---
+++ WeatherMapDemo/WeatherMapDemo/DFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DFAppDelegate.h
+//  WeatherMapDemo
+//
+//  Created by Dmitry Fedorov on 19.06.14.
+//  Copyright (c) 2014 Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeatherMapDemo/WeatherMapDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeatherMapDemo
+//
+//  Created by Dmitry Fedorov on 19.06.14.
+//  Copyright (c) 2014 Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DFAppDelegate class]));
+    }
+}
